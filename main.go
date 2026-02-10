4dbbcb1b02b2de6a02f11403d7663d291a88cd1f
hopesain <hopessain@gmail.com>
2026-02-10 11:41:28 +0200

    loaded the env variables in the init function.

--- main.go
+++ main.go
@@ -2,7 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	err :=godotenv.Load()
+	if err != nil {
+		slog.Error("Unable to load the env variables", "error", err)
+		os.Exit(1)
+	}
+}
 
 func main() {
 	fmt.Println("Hello world.")
