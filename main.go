acfa6a2494986a8024a79ea1232d639104a2c19a
hopesain <hopessain@gmail.com>
2026-02-11 21:47:16 +0200

    testing the topup merchant account endpoint.

--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
 	"log/slog"
 	"os"
 
-	"github.com/hopesain/onekhusa-client/clients"
-	"github.com/hopesain/onekhusa-client/models"
 	"github.com/hopesain/onekhusa-client/services"
 	"github.com/joho/godotenv"
 )
@@ -22,44 +18,50 @@ func init() {
 }
 
 func main() {
+	//Get Access Token
 	token, _ := services.GetAccessToken()
 	fmt.Println(token)
 
-	resp, err := services.TopupMerchantAccount(token)
+	// TopUp Merchant Account
+	response, err := services.TopupMerchantAccount(token)
 	if err != nil {
-		log.Fatalf("Topup failed: %v", err)
-	}
-
-	fmt.Printf("Topup Response:\n%+v\n", resp)
-
-	accountNumber := services.GetMerchantAccountNumber()
-	userEmail := services.GetAdminEmail()
-
-	var batchRequest = models.BatchJSONUploadRequest{
-		Header: models.HeaderSection{
-			MerchantAccountNumber: accountNumber,
-			IsBatchScheduled:      false,
-			ScheduledDate:         "2025-09-19",
-			CapturedBy:            userEmail,
-		},
-		Transactions: []models.TransactionsSection{
-			{
-				BeneficiaryName:          "John Phiri",
-				ConnectorID:              221500,
-				BeneficiaryAccountNumber: "12345678",
-				TransactionDescription:   "Salary Payment",
-				TransactionAmount:        45000,
-				SourceReferenceNumber:    "QKAYXD208367",
-			},
-		},
-	}
-
-	batchResponse, err := clients.UploadBatchJSONClient(batchRequest)
-	if err != nil {
-		slog.Error("failed to upload json", "error", err)
+		slog.Error("Unable to topup merchant account", "error", err)
 		return
 	}
-	pretty, _ := json.MarshalIndent(batchResponse, "", "  ")
-	fmt.Println(string(pretty))
+
+	fmt.Println("Message:  ", response)
+
+	/*
+
+		accountNumber := services.GetMerchantAccountNumber()
+		userEmail := services.GetAdminEmail()
+
+		var batchRequest = models.BatchJSONUploadRequest{
+			Header: models.HeaderSection{
+				MerchantAccountNumber: accountNumber,
+				IsBatchScheduled:      false,
+				ScheduledDate:         "2025-09-19",
+				CapturedBy:            userEmail,
+			},
+			Transactions: []models.TransactionsSection{
+				{
+					BeneficiaryName:          "John Phiri",
+					ConnectorID:              221500,
+					BeneficiaryAccountNumber: "12345678",
+					TransactionDescription:   "Salary Payment",
+					TransactionAmount:        45000,
+					SourceReferenceNumber:    "QKAYXD208367",
+				},
+			},
+		}
+
+		batchResponse, err := clients.UploadBatchJSONClient(batchRequest)
+		if err != nil {
+			slog.Error("failed to upload json", "error", err)
+			return
+		}
+		pretty, _ := json.MarshalIndent(batchResponse, "", "  ")
+		fmt.Println(string(pretty))
+	*/
 
 }
