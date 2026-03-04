28e5a2f2d425916f34d3e49c584ae144780f4cd6
hopesain <hopessain@gmail.com>
2026-03-04 22:31:03 +0200

    access tokens models

--- internal/authentication/models.go
+++ internal/authentication/models.go
@@ -1,1 +1,14 @@
 package authentication
+
+type AccessTokenRequest struct {
+	APIKey                string `json:"apiKey"`
+	APISecret             string `json:"apiSecret"`
+	OrganizationID        string `json:"organisationId"`
+	MerchantAccountNumber int    `json:"merchantAccountNumber"`
+}
+
+type AccessTokenResponse struct {
+	AccessToken     string `json:"accessToken"`
+	ExpiresOn       string `json:"expiresOn"`
+	ExpiryInMinutes int    `json:"expiryInMinutes"`
+}
