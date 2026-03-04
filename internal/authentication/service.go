8cd4068cde453602ab188b8153b8e6a03e05fb9c
hopesain <hopessain@gmail.com>
2026-03-04 22:42:44 +0200

    refactored the access token service.

--- internal/authentication/service.go
+++ internal/authentication/service.go
@@ -1,1 +1,60 @@
 package authentication
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"log/slog"
+	"net/http"
+	"time"
+)
+
+func GetAccessToken(input AccessTokenRequest) (AccessTokenResponse, error) {
+	url := "https://api.onekhusa.com/sandbox/v1/account/getAccessToken"
+
+	payload := input
+
+	jsonPayload, err := json.Marshal(payload)
+	if err != nil {
+		slog.Error(
+			"Failed to marshal payload",
+			"Context", "Get Access Token from oneKhusa",
+			"Error", err,
+		)
+		return AccessTokenResponse{}, err
+	}
+
+	request, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonPayload))
+	if err != nil {
+		slog.Error(
+			"Failed to create a new request",
+			"Context", "Get Access Token from oneKhusa",
+			"Error", err,
+		)
+		return AccessTokenResponse{}, err
+	}
+
+	request.Header.Set("Accept-Language", "en")
+	request.Header.Set("Content-Type", "application/json")
+
+	client := http.Client{Timeout: 30 * time.Second}
+	response, err := client.Do(request)
+	if err != nil {
+		slog.Error("HTTP request failed", "error", err)
+		return AccessTokenResponse{}, err
+	}
+
+	defer response.Body.Close()
+
+	bodyBytes, _ := io.ReadAll(response.Body)
+
+	var output AccessTokenResponse
+
+	err = json.Unmarshal(bodyBytes, &output)
+	if err != nil {
+		slog.Error("Failed decoding", "error", err)
+		return AccessTokenResponse{}, err
+	}
+
+	return output, nil
+}
