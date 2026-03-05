a5d089bc0c54679f9aa5e666b9dec9a3082f0b7b
hopesain <hopessain@gmail.com>
2026-03-05 19:14:46 +0200

    created the client for the upload service

--- main.go
+++ main.go
@@ -6,12 +6,10 @@ import (
 	"log/slog"
 	"os"
 
-	"github.com/hopesain/onekhusa-client/clients"
 	"github.com/hopesain/onekhusa-client/internal/authentication"
 	"github.com/hopesain/onekhusa-client/internal/disbursements/topup"
-	"github.com/hopesain/onekhusa-client/models"
+	"github.com/hopesain/onekhusa-client/internal/disbursements/uploads"
 	"github.com/hopesain/onekhusa-client/pkg/utils"
-	"github.com/hopesain/onekhusa-client/services"
 	"github.com/joho/godotenv"
 )
 
@@ -90,18 +88,17 @@ func main() {
 	fmt.Println(string(prettyAccountTopupResponse))
 
 	//Upload Batch JSON
+	accountNumber := merchantAccountNumber
+	userEmail := adminEmail
 
-	accountNumber := services.GetMerchantAccountNumber()
-	userEmail := services.GetAdminEmail()
-
-	var batchRequest = models.BatchJSONUploadRequest{
-		Header: models.HeaderSection{
+	var batchRequestInputData = uploads.BatchJSONUploadRequest{
+		Header: uploads.HeaderSection{
 			MerchantAccountNumber: accountNumber,
 			IsBatchScheduled:      false,
 			ScheduledDate:         nil,
 			CapturedBy:            userEmail,
 		},
-		Transactions: []models.TransactionsSection{
+		Transactions: []uploads.TransactionsSection{
 			{
 				BeneficiaryName:          "CASEY CONROY",
 				ConnectorID:              221500,
@@ -113,12 +110,9 @@ func main() {
 		},
 	}
 
-	batchResponse, err := clients.UploadBatchJSONClient(batchRequest)
-	if err != nil {
-		slog.Error("failed to upload json", "error", err)
-		return
-	}
-	pretty, _ := json.MarshalIndent(batchResponse, "", "  ")
+	uploadBatchJSONResponse, err := uploads.UploadBatchJSONService(accessToken, batchRequestInputData)
+	pretty, _ := json.MarshalIndent(uploadBatchJSONResponse, "", "  ")
+	fmt.Println("Upload Batch JSON Service")
 	fmt.Println(string(pretty))
 
 }
